6e7856848671f7f519a4a85dedab391e18045e15
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-04 21:21:41 +0000

    A medio camino

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,40 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
+
+    # Inicio  de sesión
+@api.route('/login',  methods=['POST'])
+def user_login():
+    email=request.json.get('email')
+    password=request.json.get('password')
+    user=User.query.filter(User.email==email).first()
+    # No encuentro Usuario
+
+    if user == None:
+        print(("Correo invalido"))
+        return jsonify({"msg":"Inicio de sesion invalido"}), 401
+    # Validacion de la clave
+
+    if user.password==password:
+        print("Clave ok")
+        access_token=create_access_token(identity=user.id)
+        refresh_token=create_refresh_token(identity=user.id)
+        return jsonify({"token":access_token, "refresh":refresh_token}), 200
+
+    else:
+    # Clave Invalida
+        print(("Clave invalida"))
+        return jsonify({"msg":"Inicio de sesion invalido"}), 401
+
+@api.route('/userdata')
+@jwt_required()
+def get_user_data():
+    user_id=get_jwt_identity()
+    user=User.query.get(user_id)
+    return jsonify(user.serialize())
 
 
 @api.route('/hello', methods=['POST', 'GET'])
